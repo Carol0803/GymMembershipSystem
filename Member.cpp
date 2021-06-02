3c5456145ed7825a45c8ae0efc43a6c4c6e12a7b
Carol0803 <carolwong.0803@gmail.com>
2021-06-02 15:41:32 +0800

    2/6 - add enqueueMenber

--- Member.cpp
+++ Member.cpp
@@ -1,14 +1,48 @@
+#include<iostream>
 #include "Member.h"
+using namespace std;
 
-template <class MyData>
-Member<MyData>::Member() {
+Member::Member() {
 	front = NULL;
 	rear = NULL;
-	total = 0;
+	count = 0;
 }
 
-template <class MyData>
-void Member<MyData>::addNew() {
+Member::~Member() {}
+
+void Member::enqueueMember() {
+	cout << "\n\tNew member registration\n";
+
+	Node* N = new Node;
+
 	cout << "Enter name: ";
-	cin >> memberID;
+	cin >> N->data.name;
+
+	cout << "Enter IC Number: ";
+	cin >> N->data.ic;
+
+	//generate age
+
+	cout << "Enter gender (M-Male / F-Female): ";
+	cin >> N->data.gender;
+
+	cout << "Choose subscription type (M-Monthly / A-Annually): ";
+	cin >> N->data.type;
+	//get expired date
+
+	cout << "Enter height: ";
+	cin >> N->data.height;
+
+	cout << "Enter weight: ";
+	cin >> N->data.weight;
+
+	N->next = NULL;
+
+	if (!Empty())
+		rear->next = N;
+	else
+		front = N;
+
+	count++;
+	rear = N;
 }
