1c0ce818545c44e1b81202077d07f2f098c0da93
Carol0803 <carolwong.0803@gmail.com>
2021-06-13 22:16:02 +0800

    report - main program

--- GymMembership.cpp
+++ GymMembership.cpp
@@ -1,3 +1,19 @@
+/*
+Project Title: Gym Membership System
+Team members : Wong Kak Lok (B032010436)
+               Muhammad Haziq bin Mohd Hapiz (B032010199) 
+Compiler: Visual Studio 2019
+
+Main Program File (GymMembership.cpp): 
+The main program involve two function, displayMainMenu() and main() function.
+The displayMainMenu() function list out the menu of operation could be done by the program and instruction to use the program.
+In main() function, an object called New is declared under class Member.
+Then, there is a do-while loop.
+In the do-while loop, the displayMainMenu() function is called and user require to enter their option.
+The input of user will call respective function to undergo the operation.
+The loop will continue until 0(Exit) is entered.
+*/
+
 #include <iostream>
 #include <string>
 #include "Member.h"
@@ -19,7 +35,6 @@ int main()
 
     New.loadData();
 
-
     do {
         displayMainMenu();
         cin >> selectMenu;
@@ -30,24 +45,24 @@ int main()
             cout << "\nExiting Program....\n";
             break;
         }
-        else if (selectMenu == 1) {
+        else if (selectMenu == 1) { //
             New.enqueueMember();
         }
         else if (selectMenu == 2) {
             New.addItem();
         }
-        else if (selectMenu == 3) { //renew 
+        else if (selectMenu == 3) { 
             New.renewSubs();
         }
         else if (selectMenu == 4) {
             New.simpleSort();
             New.displaySortedList();
         }
-        else if (selectMenu == 5) { // view expired namelist
+        else if (selectMenu == 5) { 
             New.displayExpired();
         }
         else {}
-    } while (selectMenu > -1 && selectMenu < 6);
+    } while (selectMenu != 0);
 
     return 0;
 }
