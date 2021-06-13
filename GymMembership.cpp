d0ed144c2916853408eb69194299a5eab6f87aeb
Haziq Hapiz <zyqq.dev@gmail.com>
2021-06-13 21:42:22 +0800

    fix

--- GymMembership.cpp
+++ GymMembership.cpp
@@ -40,20 +40,20 @@ int main()
             New.renewSubs();
         }
         else if (selectMenu == 4) {
+            New.displaySortedList();
+        }
+        else if (selectMenu == 5) { // view expired namelist
             int c;
             do {
                 displayList();
                 cin >> c;
                 if (c == 1) {
-                    New.displaySortedList();
-                }
-                if (c == 2) {
                     New.displayMember(1);
                 }
+                if (c == 2) {
+                    New.displayMember(0);
+                }
             } while (c < 1 || c > 2);
-        }
-        else if (selectMenu == 5) { // view expired namelist
-            New.displayMember(0);
         }
         else {}
     } while (selectMenu > -1 && selectMenu < 6);
@@ -63,8 +63,8 @@ int main()
 
 void displayList() {
     cout << "\nSelect what to display:";
-    cout << "\n1.\tView all";
-    cout << "\n2.\tView active member only";
+    cout << "\n1.\tView active membership only";
+    cout << "\n2.\tView expired membership only";
     cout << "\nYour selection:";
 }
 
@@ -73,8 +73,8 @@ void displayMainMenu() {
     cout << "\n1.\tNew member registration\n";
     cout << "2.\tProcess registration\n";
     cout << "3.\tRenew subscription\n";
-    cout << "4.\tView Member namelist\n";
-    cout << "5.\tView expired member namelist\n";
+    cout << "4.\tView all member namelist\n";
+    cout << "5.\tView active/expired member namelist\n";
     cout << "0.\tExit program.\n";
     cout << "\nYour selection: ";
 }
